f0018f98aafc0635557b72f71b4512fd6a114fd7
Ashar341 <danilloya341@gmail.com>
2023-03-05 22:32:03 -0600

    Hacer parte superior de la menu visible

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -24,4 +26,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('resume/', include('resume.urls')),
     path('thesaurus/', include('thesaurus.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
