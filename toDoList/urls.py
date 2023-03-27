3d65b6e27c9b3d0a0e5217741eedc2a9f722fc09
Ashar341 <danilloya341@gmail.com>
2023-03-27 14:39:43 -0600

    to do list finalizado y funcionando, agregado a carrusel, agregando mas detalles CSS

--- toDoList/urls.py
+++ toDoList/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.toDoList.as_view(), name='toDoList')
+    path('',views.toDoList, name='toDoList')
 ]
--- toDoList/views.py
+++ toDoList/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponseRedirect
+
+
+
+list = ['Create a To Do List']
+message = ''
 
 # Create your views here.
+def toDoList(request):
+    if request.method == 'POST':
+        item = request.POST['item'] # get the new item from the form
 
-class toDoList(generic.TemplateView):
-<<<<<<< HEAD
-    my_list = ["task1", "task2"]
-=======
->>>>>>> main
-    template_name = 'toDoList.html'
+        if item.strip():
+            # add the item to the list or database
+            list.append(item)
+        else:
+            message = 'Submit an Entry'
+    return render(request, 'toDoList.html',{'my_list':list})
+    
+
+def list_view(request):
+    # retrieve the list or database
+    context = {'list': list} # pass the list to the template
+    return render(request, 'toDoList.html', context)
